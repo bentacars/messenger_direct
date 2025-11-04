525c00e3ffffa0c16168e71d2f87c9ca5b3214e5
bentacars <bentacars88@gmail.com>
2025-11-04 17:49:26 +0800

    Update prompts.js

--- api/lib/prompts.js
+++ api/lib/prompts.js
@@ -1,1 +1,14 @@
+import { ToneB } from './tone/pack.B.js';
 
+export const P = {
+  greetNew: ToneB.greetNew,
+  greetReturning: ToneB.greetReturning,
+  askPlan: ToneB.ask.plan,
+  askLocation: ToneB.ask.location,
+  askBody: ToneB.ask.body,
+  askTrans: ToneB.ask.trans,
+  askBudgetCash: ToneB.ask.budgetCash,
+  ackNoted: ToneB.acks.noted,
+  ackGotIt: ToneB.acks.gotIt,
+  summaryIntro: ToneB.summaryIntro
+};
