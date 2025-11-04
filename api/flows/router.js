50d89f7f83acca2a20588b3dc1b370a1d8c0b900
bentacars <bentacars88@gmail.com>
2025-11-04 17:51:22 +0800

    Update router.js

--- api/flows/router.js
+++ api/flows/router.js
@@ -1,1 +1,7 @@
+import { qualifierTurn } from './qualifier.js';
 
+export async function handleTurn(session, parsed) {
+  // For Part 1 we only support Phase 1
+  // Later we’ll call middleware (FAQ), offers, car-knowledge, cash/fin flows, etc.
+  return qualifierTurn(session, parsed);
+}
