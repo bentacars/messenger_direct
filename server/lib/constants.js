0a077214c427b4e9d9199856f3d01c905784e2da
bentacars <bentacars88@gmail.com>
2025-11-06 00:31:01 +0800

    Create constants.js

---
+++ server/lib/constants.js
@@ -0,0 +1,7 @@
+// server/lib/constants.js
+
+export const SESSION_TTL_DAYS = 7; // expire after 7 days of inactivity
+export const FB_GRAPH_API = "https://graph.facebook.com/v17.0";
+export const FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN;
+export const OPENAI_MODEL = process.env.OPENAI_MODEL || "gpt-4.1-mini";
+export const INVENTORY_API_URL = process.env.INVENTORY_API_URL;
